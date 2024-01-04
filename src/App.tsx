348149ad84ab134ad1275366805cfcea6b632f63
Prathamesh Kokate <pkokate@algoanalytics.com>
2024-01-04 16:01:00 +0530

    Class scoping in Navbar.css

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import Slider from './components/Slider';
+
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
+     <br /><br /><br /><br /><br /><br />
+     <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum explicabo sunt nostrum incidunt dicta recusandae unde quo iure rem harum facilis temporibus, vitae magni aut officia. Et eius consequuntur ducimus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates ea, cumque magnam provident nam ratione ex suscipit blanditiis at itaque commodi saepe rem accusamus quas assumenda est! Incidunt, quae laboriosam. </p>
+     <br /><br /><br /><br /><br /><br /><br /><br /><br />
+     <Slider/>
+     <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+     <h2>hi</h2>
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,30 +3,42 @@ import './Navbar.css'
 
 export default function Navbar() {
   return (
-    <nav className='container'>
-      <ul className='nav-list'>
-        <li className='logo' >
-            <img src="https://aksha.algoanalytics.com/aksha_logo.svg" alt="" />
-        </li>
-        <div className="link-container">
-            <li className="nav-item">
-                <a href="/" className="link">Home</a>
+    <nav className="Nav">
+        <div className='container'>
+          <ul className='nav-list'>
+            <li className='logo' >
+                <img src="https://aksha.algoanalytics.com/aksha_logo.svg" alt="" />
+                <h2>Aksha</h2>
             </li>
-            <li className="nav-item">
-                <a href="/" className="link">Home</a>
+            <div className="link-container">
+                <li className="nav-item">
+                    <a href="/" className="link">Home</a>
+                </li>
+                <li className="nav-item">
+                    <a href="/" className="link">Features</a>
+                </li>
+                <li className="nav-item">
+                    <a href="/" className="link">Applications</a>
+                </li>
+                <li className="nav-item">
+                    <a href="/" className="link">Case studies</a>
+                </li>
+                <li className="nav-item">
+                    <a href="/" className="link">Testimonials</a>
+                </li>
+                <li className="nav-item">
+                    <a href="/" className="link">Clients</a>
+                </li>
+                <li className="nav-item">
+                    <a href="/" className="link">About Us</a>
+                </li>
+            </div>
+            <li className='right' >
+                <button className='contact' >Get in touch</button>
+                <img className='right-logo' src="https://aksha.algoanalytics.com/images/image_54.svg" alt="" />
             </li>
-            <li className="nav-item">
-                <a href="/" className="link">Home</a>
-            </li>
-            <li className="nav-item">
-                <a href="/" className="link">Home</a>
-            </li>
+          </ul>
         </div>
-        <li className='right' >
-            <button className='contact' >Get in touch</button>
-            <img className='right-logo' src="https://aksha.algoanalytics.com/images/image_54.svg" alt="" />
-        </li>
-      </ul>
     </nav>
   )
 }
